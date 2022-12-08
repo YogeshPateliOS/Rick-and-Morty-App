fcd8585b8dd6087c337e3fd9c6e4e460f1c3d08f
Yogesh Patel <yogeshp614@gmail.com>
2022-12-08 16:18:52 -0500

    Initial Commit

---
+++ Rick and Morty App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick and Morty App
+//
+//  Created by Yogesh Patel on 08/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
